3c042bcb4a1961a865324ea5977b71ab937f3580
Ayşegül ÖZMEN <aysegulozmn@outlook.com>
2023-11-14 20:36:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿for (int sayac = 0; sayac < 5; sayac++)
+{
+    Console.WriteLine("*****");
+}
+
+Console.WriteLine("\n");
+
+for (int sayac = 0; sayac < 10; sayac++)
+{
+    Console.WriteLine("***");
+}
+
+Console.WriteLine("\n");
+
+for (int counter = 5; counter < 8; counter++)
+{
+    Console.WriteLine("*****");
+    Console.WriteLine("\n");
+}
